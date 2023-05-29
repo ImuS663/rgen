3fbb899b59cee0d212dc6d416341cd9cc1179ac9
Maksym Nedvyha <imus663@gmail.com>
2023-05-29 20:10:47 +0300

    feat: added string generator with charset

--- rgen.go
+++ rgen.go
@@ -1,8 +1,24 @@
 package rgen
 
+import (
+	"math/rand"
+	"time"
+)
+
 const (
 	latinLowercaseCharset = "abcdefghijklmnopqrstuvwxyz"
 	latinUppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	numberCharset         = "0123456789"
 	specialCharset        = "!@#$%^&*/+-="
 )
+
+var random *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func stringWithCharset(length uint, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[random.Intn(len(charset))]
+	}
+
+	return string(b)
+}
