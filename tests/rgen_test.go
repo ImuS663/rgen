c6a3ad9277da50bea8603e836e10d934568f6f94
Maksym Nedvyha <imus663@gmail.com>
2023-05-29 20:55:45 +0300

    test: added test of strings to charsets

--- tests/rgen_test.go
+++ tests/rgen_test.go
@@ -3,7 +3,15 @@ package rgen
 import (
 	"bytes"
 	"github.com/ImuS663/rgen"
+	"strings"
 	"testing"
+)
+
+const (
+	latinLowercaseCharset = "abcdefghijklmnopqrstuvwxyz"
+	latinUppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numberCharset         = "0123456789"
+	specialCharset        = "!@#$%^&*/+-="
 )
 
 func TestBytesLength(t *testing.T) {
@@ -44,5 +52,38 @@ func TestStringSpecialLength(t *testing.T) {
 
 	if len(s) != 8 {
 		t.Fatalf("incorrect string length: %v", len(s))
+	}
+}
+
+func TestStringCharset(t *testing.T) {
+	charset := latinLowercaseCharset + latinUppercaseCharset + numberCharset
+	s := rgen.String(15)
+
+	for i := range s {
+		if !strings.Contains(charset, string(s[i])) {
+			t.Fatalf("string contains a incorrect char: %v", string(s[i]))
+		}
+	}
+}
+
+func TestStringLatinOnlyCharset(t *testing.T) {
+	charset := latinLowercaseCharset + latinUppercaseCharset
+	s := rgen.StringLatinOnly(28)
+
+	for i := range s {
+		if !strings.Contains(charset, string(s[i])) {
+			t.Fatalf("string contains a incorrect char: %v", string(s[i]))
+		}
+	}
+}
+
+func TestStringSpecialCharset(t *testing.T) {
+	charset := latinLowercaseCharset + latinUppercaseCharset + numberCharset + specialCharset
+	s := rgen.StringSpecial(8)
+
+	for i := range s {
+		if !strings.Contains(charset, string(s[i])) {
+			t.Fatalf("string contains a incorrect char: %v", string(s[i]))
+		}
 	}
 }
